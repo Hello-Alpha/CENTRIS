7481ce870d1bda56f5bda400224927238954555a
colored-dye <baoyuntai@outlook.com>
2022-05-29 16:53:18 +0800

    :pencil: PyPI installer

---
+++ collector/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='pypi_downloader',
+   version='1.0',
+   description='A small utility to download packages from PyPi',
+   license="MIT",
+   long_description=long_description,
+   author='Andreas Stamminger',
+   author_email='andreas.stamminger@pm.me',
+   scripts=['bin/sync_package_cache'],
+   url="https://github.com/astamminger/pypi_downloader",
+   packages=['pypi_downloader'],
+   install_requires=['requests', 'click', 'configparser'],
+)
---
+++ collector/tar.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#!/bin/bash
+#目录格式为
+# src
+#     lxml
+#      lxml1.0.tar.gz
+#      lxml1.1.tar.gz
+#     request
+#      request1.0.tar.gz
+#      request1.1.tar.gz
+#      request1.2.tar.gz
+#     scrapy
+
+
+ls > dir.log #以package为名的文件夹，写入dir.log文件中
+for i in $(cat dir.log)
+do 
+    if [ -d $i ]; then
+    cd $i
+    echo $i
+    ls *.tar.gz > ls_gz.log
+    for k in $(cat ls_gz.log)   
+    do
+        tar -zxf $k & > /dev/null   
+    done
+    rm ls_gz.log
+    cd ..
+    fi
+done  
+rm dir.log
+
