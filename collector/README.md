cb66094552e4fb04186c0d9c254cb4bf9451d4c0
Diego Brando <3190103302@zju.edu.cn>
2022-05-30 17:33:08 +0800

    repair tar.sh

--- collector/README.md
+++ collector/README.md
@@ -1,15 +1,17 @@
 ## 使用方法
+
 编写配置文件，示例如config.
 
-(new) 把文件整合到了`main.py`里，直接`python main.py config`就可以了\~
+(new) 把文件整合到了`main.py`里，直接`python3 main.py config`就可以了\~
 
 结果放在./cache中.
-在cache文件夹下`sh tar.sh`解压缩.
+在collector文件夹下`./tar.sh`解压缩.
 
 ``Failed to request xxx''表示request失败，会被放进redo.log里。
 
 Invalid package''表示包不存在或请求错误，不会重试。
 
 ## 注意事项
+
 不能Ctrl-C中断，只能直接关掉命令行窗口或kill(QAQ).
 
--- collector/tar.sh
+++ collector/tar.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
-#!/bin/bash
 #目录格式为
-# cache
-#     lxml
-#      lxml1.0.tar.gz
-#      lxml1.1.tar.gz
-#     request
-#      request1.0.tar.gz
-#      request1.1.tar.gz
-#      request1.2.tar.gz
-#     scrapy
+#collector
+    # cache
+    #     lxml
+    #      lxml1.0.tar.gz
+    #      lxml1.1.tar.gz
+    #     request
+    #      request1.0.tar.gz
+    #      request1.1.tar.gz
+    #      request1.2.tar.gz
+    #     scrapy
 
-
+cd cache
 ls > dir.log #以package为名的文件夹，写入dir.log文件中
 for i in $(cat dir.log)
 do 
     if [ -d $i ]; then
     cd $i
     echo $i
-    ls | grep tar.gz > ls_gz.log
-    ls | grep tar.bz2 > ls_bz2.log
-    ls | grep .zip > ls_zip.log
+    ls *.tar.gz 1>ls_gz.log 2>tar_error.log
     for k in $(cat ls_gz.log)   
     do
-        tar -zxf $k & > /dev/null   
-    done
-    for k in $(cat ls_bz2.log)
-    do
-        tar -zxf $k & > /dev/null
-    done
-    for k in $(cat ls_zip.log)
-    do
-        unzip -zxf $k & > /dev/null
+        tar -zxf $k   
+        rm $k
     done
     rm ls_gz.log
-    rm ls_bz2.log
-    rm ls_zip.log
+    rm tar_error.log
     cd ..
     fi
 done  
