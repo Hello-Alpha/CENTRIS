1059acbbfffc079bb0311a63077f1dac9d7bd26d
colored-dye <baoyuntai@outlook.com>
2022-06-02 14:10:29 +0800

    多线程

--- collector/old_files/pypi_downloader/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-
-from pypi_downloader.pypi_downloader import (build_package_cache,
-                                             create_directory)
